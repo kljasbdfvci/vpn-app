f22894b3598ccd251706ac647d57e22ab7b937e3
arminmokri <arminmokri@gmail.com>
2022-10-24 12:15:57 +0330

    new

--- bin/init.sh
+++ bin/init.sh
@@ -9,36 +9,47 @@ flag_rebbot=0
 ### copy tmp os files to os
 res_cps=0
 tmp_os_file_path="$this_dir_path/../os"
-files=$(find $tmp_os_file_path -type f)
+files=$(find $tmp_os_file_path)
 for tmp_os_file in $files
 do
     os_file=$(echo $tmp_os_file | sed "s/^${tmp_os_file_path//\//\\\/}//")
-    tmp_os_file_md5sum=$(md5sum $tmp_os_file | cut -d ' ' -f 1 | tr -d '\n')
-    os_file_md5sum=""
-    if [ -f "$os_file" ]; then
-        os_file_md5sum=$(md5sum $os_file | cut -d ' ' -f 1 | tr -d '\n')
+
+    if [[ -f $tmp_os_file ]]; then
+        tmp_os_file_md5sum=$(md5sum $tmp_os_file | cut -d ' ' -f 1 | tr -d '\n')
+        os_file_md5sum=""
+        if [ -f "$os_file" ]; then
+            os_file_md5sum=$(md5sum $os_file | cut -d ' ' -f 1 | tr -d '\n')
+        fi
+        if [ "$tmp_os_file_md5sum" != "$os_file_md5sum" ]; then
+            flag_rebbot=1
+            parent=$(dirname $os_file)
+            filename=$(basename $os_file)
+            # if process is running
+            if pgrep -x $filename > /dev/null
+            then
+                killall $filename
+                sleep 1
+            fi
+            # do copy
+            mkdir -p $parent && cp $tmp_os_file $os_file
+            res_cp=$?
+            if [ $res_cp -ne 0 ]; then
+                res_cps=1
+            fi
+            # if systemd file change
+            if [ "$parent" == "/lib/systemd/system" ]; then
+                systemctl daemon-reload
+                sleep 1
+                systemctl enable $filename
+            fi
+        fi
+    elif [[ -d $tmp_os_file ]]; then
+        if [[ ! -e $os_file ]]; then
+            flag_rebbot=1
+            mkdir -p $os_file
+        fi
     fi
-    if [ "$tmp_os_file_md5sum" != "$os_file_md5sum" ]; then
-        flag_rebbot=1
-        parent=$(dirname $os_file)
-        filename=$(basename $os_file)
-        if pgrep -x $filename > /dev/null
-        then
-            killall $filename
-            sleep 1
-        fi
-        mkdir -p $parent && cp $tmp_os_file $os_file
-        res_cp=$?
-        if [ $res_cp -ne 0 ]; then
-            res_cps=1
-        fi
-        # if systemd file change
-        if [ "$parent" == "/lib/systemd/system" ]; then
-            systemctl daemon-reload
-            sleep 1
-            systemctl enable $filename
-        fi
-    fi
+
 done
 if [ $res_cps -eq 0 ]; then
     echo "copy os files successful."
