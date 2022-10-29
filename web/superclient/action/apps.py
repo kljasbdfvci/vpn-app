ec8ecf17ebb4de54abf3091c6892fdde78030010
arminmokri <arminmokri@gmail.com>
2022-10-29 19:09:22 +0330

    hi

--- web/superclient/action/apps.py
+++ web/superclient/action/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class ActionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'superclient.action'
