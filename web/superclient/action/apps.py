614fc8c4c545bccd83fbaf00e324736f9ab2b61e
Amin Rostami <somthing4die@gmail.com>
2022-10-31 10:57:47 +0330

    fix run migrate when background_task table does not exists

--- web/superclient/action/apps.py
+++ web/superclient/action/apps.py
@@ -7,6 +7,22 @@ class ActionConfig(AppConfig):
     name = 'superclient.action'
 
     def ready(self):
-        print("Init Tasks...")
-        from superclient.action.task import service_checker
-        service_checker(repeat=5)
+        if db_table_exists('background_task'):
+            print("Init Tasks...")
+            from superclient.action.task import service_checker
+            service_checker(repeat=5)
+
+
+def db_table_exists(table, cursor=None):
+    try:
+        if not cursor:
+            from django.db import connection
+            cursor = connection.cursor()
+        if not cursor:
+            raise Exception
+        table_names = connection.introspection.get_table_list(cursor)
+        table_names = [table.name for table in table_names]
+    except:
+        raise Exception("unable to determine if the table '%s' exists" % table)
+    else:
+        return table in table_names
