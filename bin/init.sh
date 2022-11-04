c8fe2b0be8646e72bd714177fa6da4d1163da6a7
arminmokri <arminmokri@gmail.com>
2022-11-04 10:15:22 +0330

    hi

--- bin/init.sh
+++ bin/init.sh
@@ -45,7 +45,7 @@ else
 fi
 
 # install list of apt packages
-packages=("python3" "python3-dev" "python3-pip" "net-tools" "hostapd" "dnsmasq" "jq" "network-manager-openconnect" "openconnect" "openssl")
+packages=("python3" "python3-dev" "python3-pip" "net-tools" "hostapd" "dnsmasq" "jq" "openconnect" "openssl" "redsocks")
 for package in ${packages[@]}
 do
     if [ -z "$(dpkg -l | grep -w $package)" ]
