c04308b143ded99dd040558eef6060846052e398
arminmokri <arminmokri@gmail.com>
2022-11-06 20:40:32 +0330

    hh

--- bin/init.sh
+++ bin/init.sh
@@ -188,7 +188,7 @@ fi
 python3 -m pip install -r "$this_dir_path/../web/requirments.txt"
 python3 "$this_dir_path/../web/manage.py" migrate
 python3 "$this_dir_path/../web/manage.py" ensure_adminuser --username=admin --password=admin
-python3 "$this_dir_path/../web/manage.py" runserver 0.0.0.0:80 1>/tmp/app-web.log 2>/tmp/app-web.log.error &
+nohup python3 "$this_dir_path/../web/manage.py" runserver 0.0.0.0:80 1>/tmp/app-web.log 2>/tmp/app-web.log.error &
 
 # if anychange in os reboot
 if [ $flag_rebbot -eq 1 ]; then 
