360566e6530ce2fcd249342a0b43df37a67ee628
Amin Rostami <somthing4die@gmail.com>
2022-11-09 17:46:52 +0330

    fix mistake

--- web/superclient/settings.py
+++ web/superclient/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DB_DIR = Path("")
+DB_DIR = Path("/disk")
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#06b)2x@c_klxb5&%3jaegee6n&dto7z3-na+p^)$=0%bkfggz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['/disk']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
