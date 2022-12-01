84d147e4ef8cc2943c7a5238d32dbbfaf95a148c
arminmokri <arminmokri@gmail.com>
2022-12-01 12:38:48 +0330

    hi

--- web/superclient/vpn/admin.py
+++ web/superclient/vpn/admin.py
@@ -2,11 +2,27 @@ from django.contrib import admin
 from django import forms
 from .models import OpenVpnConfig, OpenconnectConfig, L2tpConfig, ShadowSocksConfig, V2rayConfig, V2rayUrlConfig
 
-
+class OpenconnectConfigAdminForm(forms.ModelForm):
+    class Meta:
+        model = OpenconnectConfig
+        widgets = {
+            'last_log': forms.Textarea,
+        }
+        fields = '__all__' 
 
 @admin.register(OpenconnectConfig)
 class OpenconnectConfigAdmin(admin.ModelAdmin):
-    list_display = ('name', 'enable', 'priority', 'success', 'failed')
+    form = OpenconnectConfigAdminForm
+    list_display = ['name', 'enable', 'priority', 'success', 'failed', 'host', 'port']
+    readonly_fields = ['last_log']
+
+    def get_fields (self, request, obj=None, **kwargs):
+        fields = super().get_fields(request, obj, **kwargs)
+        configuration_list = ['name', 'description', 'enable', 'priority', 'last_log']
+        for i in range(len(configuration_list)): 
+            fields.remove(configuration_list[i])
+            fields.insert(i, configuration_list[i])
+        return fields
 
 #@admin.register(OpenVpnConfig)
 #class OpenVpnConfigAdmin(admin.ModelAdmin):
@@ -24,6 +40,7 @@ class V2rayConfigAdminForm(forms.ModelForm):
     class Meta:
         model = V2rayConfig
         widgets = {
+            'last_log': forms.Textarea,
             'config_json': forms.Textarea,
             'config_url': forms.Textarea
         }
@@ -32,13 +49,22 @@ class V2rayConfigAdminForm(forms.ModelForm):
 @admin.register(V2rayConfig)
 class V2rayConfigAdmin(admin.ModelAdmin):
     form = V2rayConfigAdminForm
-    list_display = ('name', 'enable', 'host', 'port','priority', 'success', 'failed')
-    readonly_fields = ('config_url', 'config_json')
+    list_display = ['name', 'enable', 'priority', 'success', 'failed', 'host', 'port']
+    readonly_fields = ['last_log', 'config_url', 'config_json']
+
+    def get_fields (self, request, obj=None, **kwargs):
+        fields = super().get_fields(request, obj, **kwargs)
+        configuration_list = ['name', 'description', 'enable', 'priority', 'last_log']
+        for i in range(len(configuration_list)): 
+            fields.remove(configuration_list[i])
+            fields.insert(i, configuration_list[i])
+        return fields
 
 class V2rayUrlConfigAdminForm(forms.ModelForm):
     class Meta:
         model = V2rayConfig
         widgets = {
+            'last_log': forms.Textarea,
             'config_json': forms.Textarea,
             'config_url': forms.Textarea
         }
@@ -47,5 +73,13 @@ class V2rayUrlConfigAdminForm(forms.ModelForm):
 @admin.register(V2rayUrlConfig)
 class V2rayUrlConfigAdmin(admin.ModelAdmin):
     form = V2rayUrlConfigAdminForm
-    list_display = ('name', 'enable', 'config_url', 'priority', 'success', 'failed')
-    readonly_fields = ('name', 'host', 'port', 'config_json', 'v', 'protocol', 'uid', 'alter_id', 'tls', 'tls_allow_insecure', 'network', 'ws_path', 'ws_host')
+    list_display = ['name', 'enable', 'priority', 'success', 'failed', 'config_url']
+    readonly_fields = ['last_log', 'name', 'host', 'port', 'config_json', 'v', 'protocol', 'uid', 'alter_id', 'tls', 'tls_allow_insecure', 'network', 'ws_path', 'ws_host']
+
+    def get_fields (self, request, obj=None, **kwargs):
+        fields = super().get_fields(request, obj, **kwargs)
+        configuration_list = ['name', 'description', 'enable', 'priority', 'last_log']
+        for i in range(len(configuration_list)): 
+            fields.remove(configuration_list[i])
+            fields.insert(i, configuration_list[i])
+        return fields
