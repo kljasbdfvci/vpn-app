cd3b6a3b8ff8c2849702f1e4b77dcf8743ac7921
arminmokri <arminmokri@gmail.com>
2022-12-05 21:38:03 +0330

    hi

--- os/etc/system/mount.sh
+++ os/etc/system/mount.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 memory_path="/memory"
-swapfile_path="/mnt/swapfile"
 
-### Wait For swapon tmpfs
-for i in {1..300}
-do
-    ### check memory storage Mount
-    echo "check swapon tmpfs Mount try($i)"
-    if [ -n "$(swapon --show | grep $swapfile_path)" ]; then
-        break
-    fi
-   
-   ### sleep
-   sleep 1
-done
+swap_enable="no"
+swapfile_path="/mnt/swapfile"
+if [[ $swap_enable == "yes" ]]; then
+    ### Wait For swapon tmpfs
+    for i in {1..300}
+    do
+        ### check memory storage Mount
+        echo "check swapon tmpfs Mount try($i)"
+        if [ -n "$(swapon --show | grep $swapfile_path)" ]; then
+            break
+        fi
+    
+    ### sleep
+    sleep 1
+    done
+fi
 
 if [[ ! -e $memory_path ]]; then
     mkdir -p $memory_path
 fi
 
-mount -t tmpfs -o size=32M,mode=0755 tmpfs $memory_path
+mount -t tmpfs -o size=8M,mode=0755 tmpfs $memory_path
--- os/etc/system/swap.sh
+++ os/etc/system/swap.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+swap_enable="no"
 swapfile_path="/mnt/swapfile"
 swap_size=1024 # MB
 
-flag=0
-if [[ ! -f $swapfile_path ]]; then
-    flag=1
-    echo "swap file $swapfile_path not exist."
-else
-    current_swapfile_size=$(stat -c %s $swapfile_path)
-    swapfile_size=$(($swap_size * 1048576))
-    if [[ $current_swapfile_size -ne $swapfile_size ]]; then
+if [[ $swap_enable == "yes" ]]; then
+    flag=0
+    if [[ ! -f $swapfile_path ]]; then
         flag=1
-        echo "swap file $swapfile_path size changed."
+        echo "swap file $swapfile_path not exist."
+    else
+        current_swapfile_size=$(stat -c %s $swapfile_path)
+        swapfile_size=$(($swap_size * 1048576))
+        if [[ $current_swapfile_size -ne $swapfile_size ]]; then
+            flag=1
+            echo "swap file $swapfile_path size changed."
+        fi
     fi
-fi
 
-if [[ $flag -eq "1" ]]; then
-    dd if=/dev/zero of=$swapfile_path bs=1024 count=$(($swap_size * 1024))
-    chmod 600 $swapfile_path
-    mkswap $swapfile_path
-    echo "swap file $swapfile_path created."
-fi
+    if [[ $flag -eq "1" ]]; then
+        dd if=/dev/zero of=$swapfile_path bs=1024 count=$(($swap_size * 1024))
+        chmod 600 $swapfile_path
+        mkswap $swapfile_path
+        echo "swap file $swapfile_path created."
+    fi
 
-if [ -z "$(swapon --show | grep $swapfile_path)" ]; then
-    swapon $swapfile_path
-    echo "swap file $swapfile_path mounted."
+    if [ -z "$(swapon --show | grep $swapfile_path)" ]; then
+        swapon $swapfile_path
+        echo "swap file $swapfile_path mounted."
+    else
+        echo "swap file $swapfile_path is already mount."
+    fi
 else
-    echo "swap file $swapfile_path is already mount."
+    echo "swap is disabled."
 fi
+
