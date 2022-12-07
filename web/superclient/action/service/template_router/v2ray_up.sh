464337c13ce8aaf9ab3677450f458f97dff8e5f6
arminmokri <arminmokri@gmail.com>
2022-12-07 23:15:08 +0330

    hi

--- web/superclient/action/service/template_router/v2ray_up.sh
+++ web/superclient/action/service/template_router/v2ray_up.sh
@@ -173,9 +173,9 @@ if [ "$exit_code" == 0 ]; then
         fi
     elif [[ $tun2socks == "tun2socks" ]]; then
         if [[ $log == "yes" ]]; then
-            tun2socks -loglevel info -device tun://$vpn_interface -proxy socks5://$v2ray_inbounds_ip:$v2ray_inbounds_port -udp-timeout 300s &> $tun2socks_log_file &
+            tun2socks -loglevel info -device tun://$vpn_interface -proxy socks5://$v2ray_inbounds_ip:$v2ray_inbounds_port -udp-timeout 300s -tcp-auto-tuning &> $tun2socks_log_file &
         else
-            tun2socks -loglevel silent -device tun://$vpn_interface -proxy socks5://$v2ray_inbounds_ip:$v2ray_inbounds_port -udp-timeout 300s &> /dev/null &
+            tun2socks -loglevel silent -device tun://$vpn_interface -proxy socks5://$v2ray_inbounds_ip:$v2ray_inbounds_port -udp-timeout 300s -tcp-auto-tuning &> /dev/null &
         fi
     fi
 
